3c6bde873b4caf4fd1b30caf408ea90d51653da3
Leonardo Gregório de Almeida <leogreal@gmail.com>
2018-08-29 22:57:31 -0300

    Login

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { TextInput, StyleSheet, View } from "react-native";
+
+import FormRow from "../components/FormRow";
+
+export default class LoginPage extends React.Component {
+  render() {
+    return (
+      <View>
+        <FormRow>
+          <TextInput style={styles.input} placeholder="user@mail.com" />
+        </FormRow>
+        <FormRow>
+          <TextInput
+            style={styles.input}
+            placeholder="******"
+            secureTextEntry
+          />
+        </FormRow>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  input: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom: 5
+  }
+});
